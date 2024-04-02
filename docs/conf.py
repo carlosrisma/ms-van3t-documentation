228c590398bf650257246ea605a71c129eb35162
Carlos Mateo Risma Carletti <64154281+carlosrisma@users.noreply.github.com>
2024-04-02 17:04:23 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,20 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
     'breathe',
 ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
 
 # Breathe extension variables
 breathe_projects = { "ReadTheDocs-Breathe": "doxyxml/" }
